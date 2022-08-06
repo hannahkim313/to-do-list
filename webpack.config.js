72d5080b1c731753c161a7ec3247631c5e558f23
Hannah Kim <devhkim13@gmail.com>
2022-08-05 17:52:49 -0700

    Load HTMLWebpackPlugin and launch webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
     devtool: "inline-source-map",
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //       title: "To-Do Manager",
-    //     }),
-    // ],
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: "To-Do Manager",
+        }),
+    ],
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
