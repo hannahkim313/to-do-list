a8e5f72245a0f1f1389f3a4584288a031c6c0df2
Hannah Kim <devhkim13@gmail.com>
2022-08-07 10:11:47 -0700

    Break up code into smaller functions

--- src/modules/dom-creation/header.js
+++ src/modules/dom-creation/header.js
@@ -1,17 +1,24 @@
+import { Img } from "../dom-creation/elements/img";
 import logoImg from "../../img/logo.svg";
 
-const createHeader = () => {
-    const logo = document.createElement("img");
+const createLogo = () => {
+    const img = Img({ src: logoImg, alt: "Bulleted list icon" });
+    const logo = img.get();
     logo.setAttribute("class", "logo");
-    logo.src = logoImg;
-    logo.alt = "Bulleted list icon";
+    return logo;
+};
 
+const createHeading = () => {
     const heading = document.createElement("h1");
     heading.textContent = "To-Do Manager";
+    return heading;
+};
 
+const createHeader = () => {
+    const logo = createLogo();
+    const heading = createHeading();
     const header = document.createElement("header");
     header.append(logo, heading);
-
     return header;
 };
 
