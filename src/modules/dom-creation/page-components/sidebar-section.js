c486f721b55713b46611c8e9d72191dbcc5e89ae
Hannah Kim <devhkim13@gmail.com>
2022-08-09 14:13:22 -0700

    Make sidebar sections into buttons and update CSS

--- src/modules/dom-creation/page-components/sidebar-section.js
+++ src/modules/dom-creation/page-components/sidebar-section.js
@@ -1,16 +1,27 @@
 import { appendChildren } from "../../helper-functions/append-children";
 import { Img } from "../elements/img";
-import { Para } from "../elements/p";
+import { Para } from "../elements/para";
+import { Button } from "../elements/button";
 
 const _createIcon = (attributes) => Img(attributes).getImg();
 
 const _createName = (text) => Para(text).getPara();
 
+const _createSectionBtn = () => {
+    const attributes = {
+        type: "button",
+    };
+    const btn = Button(attributes).getBtn();
+    return btn;
+};
+
 const SidebarSection = (sectionInfo) => {
     const icon = _createIcon(sectionInfo.imgAttributes);
     const sectionName = _createName(sectionInfo.paraText.text);
+    const sectionBtn = _createSectionBtn();
+    appendChildren(sectionBtn, icon, sectionName);
     const section = document.createElement("li");
-    appendChildren(section, icon, sectionName);
+    appendChildren(section, sectionBtn);
     const getSection = () => section;
     return { getSection };
 };
