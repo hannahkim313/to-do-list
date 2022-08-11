d6ba569e51ef6012c1dff2a03cb2005afd82aa43
Hannah Kim <devhkim13@gmail.com>
2022-08-11 10:03:57 -0700

    Create 'Home' page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { appendChildren } from "./modules/helper-functions/append-children";
 import { createHeader } from "./modules/dom-creation/header";
 import { createSidebar } from "./modules/dom-creation/sidebar";
+import { createHomePage } from "./modules/dom-creation/home-page";
 import { createFooter } from "./modules/dom-creation/footer";
 import "./css/style.css";
 import "./css/reset.css";
@@ -8,6 +9,7 @@ import "./css/reset.css";
 const bodyElements = [
     createHeader(),
     createSidebar(),
+    createHomePage(),
     createFooter()
 ];
 
